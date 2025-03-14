01de09ede4a9481c9fc239b36e637d398b6edcfa
johannnsilva <johannn9896@gmail.com>
2025-03-15 00:59:35 +0530

    content update

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,7 @@
+export * as authApi from './auth';
+export * as billingApi from './billing';
+export * as bookingApi from './booking';
+export * as customerApi from './customer';
+export * as driverApi from './driver';
+export * as vehicleApi from './vehicle';
+export { default as ApiClient } from './client';
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './user';
+export * from './bill';
+export * from './booking';
+export * from './customer';
+export * from './driver';
+export * from './vehicle';
